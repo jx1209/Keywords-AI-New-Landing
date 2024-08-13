98989a9544f7a5629e059191a644c8cc20de45d8
YunhaoLiu1228 <yunhaol4@illinois.edu>
2024-08-13 14:18:57 -0700

    add metadata

--- src/app/blog/[blog-url]/layout.tsx
+++ src/app/blog/[blog-url]/layout.tsx
@@ -1,4 +1,42 @@
+import { getMetadata } from "@/utilities/getMetadata";
 import "@mintlify/mdx/dist/styles.css";
+import type { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { "blog-url": string };
+}): Promise<Metadata> {
+  const filePath = params["blog-url"];
+  const metadata = await getMetadata(filePath);
+  if (!metadata) {
+    return {};
+  }
+
+  return {
+    title: metadata?.title ?? "",
+    description: metadata?.description ?? "",
+    openGraph: {
+      type: "website",
+      siteName: "KeywordsAI.co",
+      title: metadata.title ?? "",
+      url: `https://keywordsai.co/blog/${filePath}`,
+      description: metadata.description ?? "",
+      images:
+        metadata?.images ??
+        `https://keywordsai-static.s3.amazonaws.com/social_media_images/social_image.png`,
+      locale: "en_US",
+    },
+    twitter: {
+      title: metadata.title ?? "",
+      description: metadata.description ?? "",
+      card: "summary_large_image",
+      images:
+        metadata?.images ??
+        `https://keywordsai-static.s3.amazonaws.com/social_media_images/social_image.png`,
+    },
+  };
+}
 
 export default function RootLayout({
     children,
