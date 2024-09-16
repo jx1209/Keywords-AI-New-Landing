e1be60862531e5b3b692151f373a75b732cf029b
YunhaoLiu1228 <yunhaol4@illinois.edu>
2024-09-16 16:24:49 -0700

    merge

--- src/app/components/ChangeLog/change_logs/index.ts
+++ src/app/components/ChangeLog/change_logs/index.ts
@@ -12,9 +12,13 @@ import log10 from './v10.json'
 import log11 from './v11.json'
 import log12 from './v12.json'
 import log13 from './v13.json'
+import log14 from './v14.json'
+import log15 from './v15.json'
+import log16 from './v16.json'
+
 // Import all your log files here
 
-const logs = [log13, log12, log11, log10, log9, log8, log7, log6, log5, log4, log3, log2, log1
+const logs = [log16, log15, log14, log13, log12, log11, log10, log9, log8, log7, log6, log5, log4, log3, log2, log1
 ] // Add all imported logs to this array
 
 export default logs
