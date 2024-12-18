066df8974c31f53145d4e7879c802d4fe665da7a
YunhaoLiu1228 <yunhaol4@illinois.edu>
2024-12-18 03:22:52 -0800

    fix login button

--- src/utilities/platformURL.jsx
+++ src/utilities/platformURL.jsx
@@ -1,74 +1,25 @@
-// const selectEndpoint = () => {
-//   const envMode = import.meta.env.MODE;
-//   if (envMode === "development") {
-//     return "https://platform-test.keywordsai.co";
-//   }
-//   const currentAddress = window.location.hostname;
+'use client';
 
-//   if (currentAddress === "test.keywordsai.co") {
-//     return "https://platform-test.keywordsai.co";
-//   } else if (currentAddress === "keywordsai.co") {
-//     return "https://platform.keywordsai.co";
-//   }
-//   return "https://platform-test.keywordsai.co";
-// };
-// export const platformURL = selectEndpoint();
-// 'use client'
-// const selectEndpoint = () => {
-  
-//   if (typeof window === 'undefined') {
-//     // Server-side code
-//     return process.env.NEXT_PUBLIC_NODE_ENV === 'development'
-//       ? 'https://platform.keywordsai.co'
-//       : 'https://platform.keywordsai.co';
-//   }
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+import { useEffect } from 'react'
 
-//   // Client-side code
-//   const currentAddress = window.location.hostname;
+export function PHProvider({ children }) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        person_profiles: 'identified_only',
+        capture_pageview: false
+      })
+    }
+  }, [])
 
-//   if (process.env.NEXT_PUBLIC_NODE_ENV === 'development' || currentAddress === 'test.keywordsai.co') {
-//     return 'https://platform.keywordsai.co';
-//   } else if (currentAddress === 'keywordsai.co') {
-//     return 'https://platform.keywordsai.co';
-//   }
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>
+}
 
-//   return 'https://platform.keywordsai.co';
-// };
-
-// export const platformURL = selectEndpoint();
-
-// const selectEndpoint = () => {
-//   let endpoint = 'https://platform.keywordsai.co';
-
-//  if (typeof window !== 'undefined') {
-//     const currentAddress = window.location.hostname;
-//     if (process.env.NEXT_PUBLIC_NODE_ENV === 'development' || currentAddress === 'test.keywordsai.co') {
-//       endpoint = 'https://platform.keywordsai.co';
-//     } else if (currentAddress === 'keywordsai.co') {
-//       endpoint = 'https://platform.keywordsai.co';
-//     }
-//   }
-// };
-
-// export const platformURL = selectEndpoint();
-
-
-
-'use client'
-import { useState, useEffect } from 'react';
+export const platformURL = 'https://platform.keywordsai.co';
 
 export function usePlatformURL() {
-  const [platformURL, setPlatformURL] = useState('https://platform.keywordsai.co');
-
-  useEffect(() => {
-    const currentAddress = window.location.hostname;
-    
-    if (process.env.NEXT_PUBLIC_NODE_ENV === 'development' || currentAddress === 'test.keywordsai.co') {
-      setPlatformURL('https://platform.keywordsai.co');
-    } else if (currentAddress === 'keywordsai.co') {
-      setPlatformURL('https://platform.keywordsai.co');
-    }
-  }, []);
-
   return platformURL;
 }
