94d5820355118eec795a05b0fbf5e46e31222525
Andydy25 <hanheli25@gmail.com>
2025-01-05 20:09:03 -0800

    live: bring back old landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import Landing from "./components/landing/landing";
 import Landing2025 from "./components/landing/Landing2025";
 
 export default function Page() {
-  return <Landing2025 />;
+  // return <Landing2025 />;
+  return <Landing />;
 }
