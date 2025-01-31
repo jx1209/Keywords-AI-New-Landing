f46bd0b6f0797c0ce19a4dd878c3dcccca03c437
root <root@DESKTOP-VF11DGT.localdomain>
2025-01-31 14:27:21 -0800

    add metadata for accel/grant detail pages

--- src/app/accelerator-grant-finder/[name]/page.tsx
+++ src/app/accelerator-grant-finder/[name]/page.tsx
@@ -1,5 +1,36 @@
 
 import AGDetailContent from "@/app/components/AcceleratorGrantFinder/AGDetailContent";
+import { Metadata } from "next";
+
+export async function generateMetadata({ params }: {
+  params: { name : string }
+}): Promise<Metadata> {
+  const resolvedParams = await params;
+  const name = resolvedParams.name;
+
+  return {
+    title: `How to apply for ${name}`,
+    description: `Learn the details about ${name} and how to apply for it.`,
+    openGraph: {
+      type: 'website',
+      title: `${name} | Keywords AI`,
+      description: `Learn the details about ${name} and how to apply for it.`,
+      url: `https://keywordsai.co/accelerator-grant-finder/${name}`,
+      siteName: 'Keywords AI',
+      images: [
+        {
+          url: 'https://keywordsai-static.s3.amazonaws.com/social_media_images/social_image.png',
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      site: '@keywordsai',
+      title: `${name} | Keywords AI`,
+      images: ['https://keywordsai-static.s3.amazonaws.com/social_media_images/social_image.png'],
+    },
+  }
+}
 
 export interface AGDetails {
     name: string;
